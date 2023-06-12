d6093eeccc9d68c351ea16b5eeb2cf403627d790
Ignat <Ignat1095@mail.ru>
2023-06-13 01:31:22 +0300

    мсавпа

---
+++ lesson_2/seminar/4.py
@@ -0,0 +1,19 @@
+# Напишите программу, которая вычисляет площадь круга и длину окружности по введённому диаметру.
+# ✔Диаметр не превышает 1000 у.е.
+# ✔Точность вычислений должна составлять не менее 42 знаков после запятой.
+from math import pi
+from decimal import Decimal, getcontext
+
+getcontext().prec = 1000  # Округление до 42
+
+
+def circle(diameter):
+    square = Decimal(diameter ** 2 / 4) * Decimal(pi)
+    circumference = Decimal(pi) * Decimal(diameter)
+    print(f'Площадь = {square}\nДлинна окр. = {circumference}')
+    print(len(str(square)), len(str(circumference)))
+
+
+print(len(str(pi)))
+# diameter = int(input("Введите диаметр: "))
+circle(12.123459876)

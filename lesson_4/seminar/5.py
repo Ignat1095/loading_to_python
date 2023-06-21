f6e2279823f60f3b4a63544bc4200b08d30d9acf
Ignat <Ignat1095@mail.ru>
2023-06-21 15:18:34 +0300

    ATM

--- lesson_4/seminar/5.py
+++ lesson_4/seminar/5.py
@@ -1,10 +1,19 @@
+# ✔Функция принимает на вход три списка одинаковой длины:
+# ✔имена str,
+# ✔ставка int,
+# ✔премия str с указанием процентов вида «10.25%».
+# ✔Вернуть словарь с именем в качестве ключа и суммой премии в качестве значения.
+# ✔Сумма рассчитывается как ставка умноженная на процент премии.
 
-def creat_dict():
-    my_dict = {}
-    start, end = map(int, input("Введите два числа через пробел: ").split())
-    if start > end:
-        start, end = end, start
-    for i in range(start, end + 1):
-        my_dict[chr(i)] = i
-    return  my_dict
-print(creat_dict())
+
+def premia(names:list[str], stavka:list[str], persents:list[str]):
+    dict_of_premia = {}
+    for index, name in enumerate(names):
+        dict_of_premia[name] = stavka[index] * float(persents[index][0:-1]) / 100
+    return dict_of_premia
+
+names = ['aaa', 'bbb', 'ccc']
+stavka = [1000, 2000, 3000]
+persents = ['10.23', '10.23', '10.23']
+
+print(premia(names, stavka, persents))

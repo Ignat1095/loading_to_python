eb4af29c9166f80feeeb6e4cf1aff2cc7f870902
Ignat <Ignat1095@mail.ru>
2023-08-17 17:21:52 +0300

    tusk 7_tusk

--- lesson_7/tusk_7/refactor_file.py
+++ lesson_7/tusk_7/refactor_file.py
@@ -2,55 +2,42 @@
 
 # Она должна:
 
-# Принимать параметр желаемое конечное имя файлов.
-# При переименовании в конце имени добавляется порядковый номер.
+# Принимать параметр желаемое конечное имя файлов. При переименовании в конце имени добавляется порядковый номер.
 # Принимать параметр количество цифр в порядковом номере.
-# Принимать параметр расширение исходного файла.
-# Переименование должно работать только для этих файлов внутри каталога.
+# Принимать параметр расширение исходного файла.  Переименование должно работать только для этих файлов внутри каталога.
 # Принимать параметр расширение конечного файла.
 
 # Принимать диапазон сохраняемого оригинального имени.
 # Например, для диапазона[3, 6] берутся буквы с 3 по 6 из исходного имени файла.
 
 # К ним прибавляется желаемое конечное имя, если оно передано.
-
 # Далее счётчик файлов и расширение.
 
-from os import chdir, getcwd
+from os import chdir, getcwd, listdir, mkdir
 from pathlib import Path
 
 
-def refactor_file(directory: str | Path = "test_dir"):
+def refactor_file(new_name: str,
+                  old_ext: str, new_ext: str = None,
+                  count_digits: int = 3,
+                  range_name: range = (3, 6),
+                  directory: str | Path = "test_dir"):
+    if directory not in listdir():
+        mkdir(directory)
+    directory = getcwd() + '\\' + directory + '\\'
     chdir(directory)
     count = 1
+    if new_ext is None:
+        new_ext = old_ext
     for file in Path(getcwd()).iterdir():
-        if file.is_dir():
-            continue
-
-        *way, name = str(file).split("\\")
-        way = "\\".join(way)
-        new_name = rename(name, count)
-        count += 1
-        file.replace(f"{way}\\{new_name}")
-
-
-def rename(name: str, count: int = 1):
-    """
-    Переименование имени если кончается на гласную,
-    Добавление порядкового номера
-    :param name:
-    :param count:
-    :return:
-    """
-    name, ext = name.split(".")
-    name, *old_number = name.split("_")
-    ext = "." + ext
-
-    if name[-1] in ['a', 'e', 'i', 'o', 'u', 'y']:
-        new_name = name[0:3] + "_" + str(count) + ext
+        if file.is_file() and file.suffix == old_ext:
+            new_file_name = f"{file.stem[range_name[0]:range_name[1]]}{new_name}_{count:0{count_digits}}{new_ext}"
+            count += 1
+            file.replace(new_file_name)
+    if count > 1:
+        print(f"Переименовано {count} файл(ов) {{old_ext}} в {{new_ext}}")
     else:
-        new_name = name + "_" + str(count) + ext
-    return new_name
+        print(f"Файлов с расширением {{old_ext}} не существует!")
 
 
-refactor_file()
+refactor_file("_new", old_ext=".txt", new_ext=".jpg")
